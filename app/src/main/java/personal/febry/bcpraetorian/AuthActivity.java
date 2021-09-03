665b2f1d6a59e63d6b7c9cd8b6cb81e76584a1ab
Febryan Grady <febryangrady@gmail.com>
2021-09-03 20:49:57 +0700

    Restart design

--- app/src/main/java/personal/febry/bcpraetorian/AuthActivity.java
+++ app/src/main/java/personal/febry/bcpraetorian/AuthActivity.java
@@ -8,35 +8,9 @@ import android.widget.Button;
 
 public class AuthActivity extends AppCompatActivity {
 
-    Button btnLogin, btnSignUp;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
-        assignAllWidget();
-        btnLogin.setOnClickListener(btnLoginListener());
-        btnSignUp.setOnClickListener(btnSignUpListener());
-    }
-
-    private View.OnClickListener btnSignUpListener() {
-        return view -> {
-            btnSignUp.setEnabled(false);
-            btnLogin.setEnabled(true);
-
-        };
-    }
-
-    private View.OnClickListener btnLoginListener() {
-        return view -> {
-            btnLogin.setEnabled(false);
-            btnSignUp.setEnabled(true);
-
-        };
-    }
-
-    private void assignAllWidget() {
-        btnLogin = findViewById(R.id.btnLogin);
-        btnSignUp = findViewById(R.id.btnSignUp);
     }
 }
